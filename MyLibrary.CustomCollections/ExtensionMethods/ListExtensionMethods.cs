11008c6d8e07342256287b5b97333526284a3ff2
Murat Aykanat <aykanatm@gmail.com>
2016-04-02 11:08:56 +0300

    Code cleanup

--- MyLibrary.CustomCollections/ExtensionMethods/ListExtensionMethods.cs
+++ MyLibrary.CustomCollections/ExtensionMethods/ListExtensionMethods.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using MyLibrary.CustomMath;
 
 namespace MyLibrary.CustomCollections.ExtensionMethods
--- MyLibrary.CustomIO/ExtensionMethods/FolderExtensionMethods.cs
+++ MyLibrary.CustomIO/ExtensionMethods/FolderExtensionMethods.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyLibrary.CustomIO.ExtensionMethods
+﻿namespace MyLibrary.CustomIO.ExtensionMethods
 {
     public static class FolderExtensionMethods
     {
