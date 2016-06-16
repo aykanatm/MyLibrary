75e79c3147066588c537b72f6e8dee84b6cb7775
Murat Aykanat <aykanatm@gmail.com>
2016-06-16 11:17:17 +0300

    Replacing special characters in a given string is now done in this extension method.

---
+++ MyLibrary.StringOperations/ExtensionMethods/StringExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.StringOperations.ExtensionMethods
+{
+    public static class StringExtensionMethods
+    {
+        public static string ReplaceSpecialCharacters(this string input)
+        {
+            return input.Replace('ı', 'i')
+                .Replace('ç', 'c')
+                .Replace('ö', 'o')
+                .Replace('ş', 's')
+                .Replace('ü', 'u')
+                .Replace('ğ', 'g')
+                .Replace('İ', 'I')
+                .Replace('Ç', 'C')
+                .Replace('Ö', 'O')
+                .Replace('Ş', 'S')
+                .Replace('Ü', 'U')
+                .Replace('Ğ', 'G');
+        }
+    }
+}
