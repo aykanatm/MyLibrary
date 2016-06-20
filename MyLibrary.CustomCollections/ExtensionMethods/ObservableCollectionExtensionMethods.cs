fed784c240aebb23218901e7bda5892b1c44e775
Murat Aykanat <aykanatm@gmail.com>
2016-06-20 14:22:04 +0300

    Extension method for observable collections added.

---
+++ MyLibrary.CustomCollections/ExtensionMethods/ObservableCollectionExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MyLibrary.CustomCollections.ExtensionMethods
+{
+    public static class ObservableCollectionExtensionMethods
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> input)
+        {
+            var output = new ObservableCollection<T>();
+            foreach (var item in input)
+            {
+                output.Add(item);
+            }
+
+            return output;
+        }
+    }
+}
