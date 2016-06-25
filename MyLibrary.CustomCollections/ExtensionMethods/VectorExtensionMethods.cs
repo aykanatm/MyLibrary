59e46eb5c40a2c89b037c1d8a6419d51001105ac
Murat Aykanat <aykanatm@gmail.com>
2016-06-25 14:23:56 +0300

    Moved vector extension methods to another class.

---
+++ MyLibrary.CustomCollections/ExtensionMethods/VectorExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary.CustomMath;
+
+namespace MyLibrary.CustomCollections.ExtensionMethods
+{
+    public static class VectorExtensionMethods
+    {
+        public static Vector ToVector(this List<double> list)
+        {
+            return new Vector(list);
+        }
+    }
+}
