f8ed1de1751738d39819890a2327dacd713692b4
Murat Aykanat <aykanatm@gmail.com>
2016-07-29 22:25:42 +0300

    Created a model to hold image properties so we dont have to reference google vision in the main project.

--- MyLibrary.GoogleVision/Annotations.cs
+++ MyLibrary.GoogleVision/Annotations.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Google.Apis.Vision.v1.Data;
+using MyLibrary.GoogleVision.Models;
 
 namespace MyLibrary.GoogleVision
 {
@@ -11,7 +12,7 @@ namespace MyLibrary.GoogleVision
         public IList<EntityAnnotation> LandmarkAnnotations { get; private set; }
         public IList<FaceAnnotation> FaceAnnotations { get; private set; }
         public SafeSearchAnnotation SafeSearchAnnotation { get; private set; }
-        public ImageProperties ImageProperties { get; private set; }
+        public ImageProperties ImageProperties { get; }
 
         public Annotations(IList<EntityAnnotation> labelAnnotations, IList<EntityAnnotation> textAnnotations, IList<EntityAnnotation> logoAnnotations,
                            IList<EntityAnnotation> landmarkAnnotations, IList<FaceAnnotation> faceAnnotations, SafeSearchAnnotation safeSearchAnnotation,
@@ -24,6 +25,23 @@ namespace MyLibrary.GoogleVision
             FaceAnnotations = faceAnnotations;
             SafeSearchAnnotation = safeSearchAnnotation;
             ImageProperties = imageProperties;
+        }
+
+        public IList<ColorRgba> GetDominantColors()
+        {
+            var colors = new List<ColorRgba>();
+            foreach (var color in ImageProperties.DominantColors.Colors)
+            {
+                colors.Add(new ColorRgba()
+                {
+                    R = color.Color.Red == null ? 0 : color.Color.Red.Value,
+                    G = color.Color.Green == null ? 0 : color.Color.Green.Value,
+                    B = color.Color.Blue == null ? 0 : color.Color.Blue.Value,
+                    A = color.Color.Alpha == null ? 0 : color.Color.Alpha.Value
+                });
+            }
+
+            return colors;
         }
     }
 }
