67e56c4edb17938fe13be115dc2802556a65a269
Murat Aykanat <aykanatm@gmail.com>
2016-08-14 10:52:46 +0300

    New wrappers added for various google vision components.

---
+++ MyLibrary.GoogleVision/Models/FaceAnnotationModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Google.Apis.Vision.v1.Data;
+
+namespace MyLibrary.GoogleVision.Models
+{
+    public class FaceAnnotationModel
+    {
+        public IList<VertexModel> Vertices { get;}
+        public IList<LandmarkModel> Landmarks { get; }
+
+        public float DetectionConfidence { get; }
+        public string AngerLikelihood { get;}
+        public string JoyLikelihood { get;}
+        public string SorrowLikelihood { get; }
+        public string HeadWearLikelihood { get; }
+        public string BlurredLikelihood { get; }
+        public string UnderexposedLikelihood { get; }
+        public string SupriseLikelihood { get; set; }
+        
+        public FaceAnnotationModel(FaceAnnotation faceAnnotation)
+        {
+            Vertices = new List<VertexModel>();
+
+            foreach (var vertex in faceAnnotation.BoundingPoly.Vertices)
+            {
+                Vertices.Add(new VertexModel(vertex));
+            }
+
+            Landmarks = new List<LandmarkModel>();
+            foreach (var landmark in faceAnnotation.Landmarks)
+            {
+                Landmarks.Add(new LandmarkModel(landmark));
+            }
+
+            DetectionConfidence = faceAnnotation.DetectionConfidence != null ? faceAnnotation.DetectionConfidence.Value : 0;
+            AngerLikelihood = faceAnnotation.AngerLikelihood;
+            JoyLikelihood = faceAnnotation.JoyLikelihood;
+            SorrowLikelihood = faceAnnotation.SorrowLikelihood;
+            HeadWearLikelihood = faceAnnotation.HeadwearLikelihood;
+            BlurredLikelihood = faceAnnotation.BlurredLikelihood;
+            UnderexposedLikelihood = faceAnnotation.UnderExposedLikelihood;
+            SupriseLikelihood = faceAnnotation.SurpriseLikelihood;
+        }
+    }
+}
---
+++ MyLibrary.GoogleVision/Models/ImagePropertiesModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Vision.v1.Data;
+
+namespace MyLibrary.GoogleVision.Models
+{
+    public class ImagePropertiesModel
+    {
+        public IList<ColorRgba> DominantColors { get; }
+        public ImagePropertiesModel(ImageProperties imageProperties)
+        {
+            DominantColors = new List<ColorRgba>();
+            foreach (var colorInfo in imageProperties.DominantColors.Colors)
+            {
+                DominantColors.Add(new ColorRgba(colorInfo));
+            }
+        }
+    }
+}
