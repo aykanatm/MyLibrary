365aee3ee4283c44156edbeaf32be1440d2cb4c4
Murat Aykanat <aykanatm@gmail.com>
2016-08-14 10:54:00 +0300

    Updated annotations based on the new wrappers.

--- MyLibrary.GoogleVision/Annotations.cs
+++ MyLibrary.GoogleVision/Annotations.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Google.Apis.Vision.v1.Data;
 using MyLibrary.GoogleVision.ExtensionMethods;
 using MyLibrary.GoogleVision.Models;
@@ -12,9 +11,9 @@ namespace MyLibrary.GoogleVision
         public string TextAnnotations { get; private set; }
         public string LogoAnnotations { get; private set; }
         public string LandmarkAnnotations { get; private set; }
-        public IList<FaceAnnotation> FaceAnnotations { get; private set; }
-        public SafeSearchModel SafeSearchAnnotation { get; private set; }
-        public ImageProperties ImageProperties { get; }
+        public IList<FaceAnnotationModel> FaceAnnotations { get;}
+        public SafeSearchModel SafeSearchAnnotation { get;}
+        public ImagePropertiesModel ImageProperties { get; }
 
         public Annotations(IList<EntityAnnotation> labelAnnotations, IList<EntityAnnotation> textAnnotations, IList<EntityAnnotation> logoAnnotations,
                            IList<EntityAnnotation> landmarkAnnotations, IList<FaceAnnotation> faceAnnotations, SafeSearchAnnotation safeSearchAnnotation,
@@ -24,26 +23,13 @@ namespace MyLibrary.GoogleVision
             TextAnnotations = textAnnotations.EntityAnnotationListToString();
             LogoAnnotations = logoAnnotations.EntityAnnotationListToString();
             LandmarkAnnotations = landmarkAnnotations.EntityAnnotationListToString();
-            FaceAnnotations = faceAnnotations;
-            SafeSearchAnnotation = safeSearchAnnotation.SafeSearchAnnotationsToModel();
-            ImageProperties = imageProperties;
-        }
+            SafeSearchAnnotation = new SafeSearchModel(safeSearchAnnotation);
+            ImageProperties = new ImagePropertiesModel(imageProperties);
 
-        public IList<ColorRgba> GetDominantColors()
-        {
-            var colors = new List<ColorRgba>();
-            foreach (var color in ImageProperties.DominantColors.Colors)
+            foreach (var faceAnnotation in faceAnnotations)
             {
-                colors.Add(new ColorRgba()
-                {
-                    R = color.Color.Red == null ? 0 : color.Color.Red.Value,
-                    G = color.Color.Green == null ? 0 : color.Color.Green.Value,
-                    B = color.Color.Blue == null ? 0 : color.Color.Blue.Value,
-                    A = color.Color.Alpha == null ? 0 : color.Color.Alpha.Value
-                });
+                FaceAnnotations.Add(new FaceAnnotationModel(faceAnnotation));
             }
-
-            return colors;
         }
     }
 }
