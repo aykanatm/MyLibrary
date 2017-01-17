73c0acdfaf7f5b46e10397ffb3e5cd02f2104b37
Murat Aykanat <aykanatm@gmail.com>
2017-01-17 12:08:16 +0300

    Removed unnecessary using statements

--- ClassOperations/PropertyCopier.cs
+++ ClassOperations/PropertyCopier.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClassOperations
+﻿namespace ClassOperations
 {
     public class PropertyCopier<TFrom, TTo> where TFrom : class
                                             where TTo : class 
