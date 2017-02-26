e08ac7ba78d1c164d1ceb1946e736fa336f05557
Murat Aykanat <aykanatm@gmail.com>
2017-02-26 13:18:06 +0300

    FilePackageWriter first iteration is complete.

---
+++ CustomIO/Packaging/FilePackage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace CustomIO.Packaging
+{
+    public class FilePackage
+    {
+        public string FilePath { get; set; }
+    }
+}
