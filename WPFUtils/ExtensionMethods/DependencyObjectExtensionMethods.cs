f56a5cfbdd65c39abee16793a0d0ffa1006692bb
Murat Aykanat <aykanatm@gmail.com>
2017-06-25 20:06:57 +0300

    New methods added.

--- WPFUtils/ExtensionMethods/DependencyObjectExtensionMethods.cs
+++ WPFUtils/ExtensionMethods/DependencyObjectExtensionMethods.cs
@@ -25,5 +25,52 @@ namespace WPFUtils.ExtensionMethods
                 }
             }
         }
+
+        public static IEnumerable<T> GetVisualChildren<T>(this Visual parent) where T : Visual
+        {
+            if (parent != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+                {
+                    var child = VisualTreeHelper.GetChild(parent, i) as Visual;
+
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in GetVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+        public static T GetVisualChild<T>(this Visual parent) where T : Visual
+        {
+            Visual child = null;
+
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                child = VisualTreeHelper.GetChild(parent, i) as Visual;
+
+                if (child != null && (child.GetType() == typeof(T)))
+                {
+                    break;
+                }
+
+                else if (child != null)
+                {
+                    child = GetVisualChild<T>(child);
+
+                    if (child != null && (child.GetType() == typeof(T)))
+                    {
+                        break;
+                    }
+                }
+            }
+            return child as T;
+        }
     }
 }
